0bcc0f725d87b085f85ef3b5924e9ad666471436
Jean-Baptiste Queru <jbq@google.com>
2012-05-16 16:44:18 -0700

    Fix build
    Change-Id: I4f915831bc16a667c8583c99aeaea25e12d678ae

--- util/symbol.c
+++ util/symbol.c
@@ -2258,7 +2258,7 @@ static u64 machine__get_kernel_start_addr(struct machine *machine)
 {
 	const char *filename;
 	char path[PATH_MAX];
-	struct process_args args;
+	struct process_args args = {0};
 
 	if (machine__is_host(machine)) {
 		filename = "/proc/kallsyms";
